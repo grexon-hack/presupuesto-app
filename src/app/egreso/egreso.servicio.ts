535488a140639658183f39dc3f7aa0c0f50bc089
grexon-hack <jsarabialugo@gmail.com>
2022-07-10 23:03:41 -0500

    finished app

--- src/app/egreso/egreso.servicio.ts
+++ src/app/egreso/egreso.servicio.ts
@@ -6,4 +6,9 @@ export class EgresoServicio {
         new Egreso("Renta Depto", 900),
         new Egreso("Ropa", 200)
     ]
+
+    eliminar(egreso: Egreso) {
+        const indice: number = this.egresos.indexOf(egreso);
+        this.egresos.splice(indice, 1);
+    }
 }
